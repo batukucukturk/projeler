98ebad602fb8e198cd90894cc8a66896379d9bec
batu han küçüktürk <bkucukturk@gmail.com>
2023-07-10 21:55:34 +0300

    taksimetre tamamlandı

--- src/Taksimetre/TaksimetreHesaplama.java
+++ src/Taksimetre/TaksimetreHesaplama.java
@@ -1,2 +1,18 @@
-package Taksimetre;public class TaksimetreHesaplama {
+package Taksimetre;
+import java.util.Scanner;
+public class TaksimetreHesaplama {
+    public static void main(String[] args) {
+        Scanner inp =new Scanner(System.in);
+        double km, ucret, baslangıc=10;
+
+        System.out.print("Lütfen gidilecek mesafeyi yazınız :");
+        km=inp.nextDouble();
+
+        ucret = km * 2.20;
+        ucret += baslangıc;
+
+        ucret = (ucret<=20) ? 20 : ucret;
+
+        System.out.print("Toplam Ücret :" + ucret);
+    }
 }

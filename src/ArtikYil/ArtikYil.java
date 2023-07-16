860eb0f016ed036bf7660305b339fc21ce51180f
batu han küçüktürk <bkucukturk@gmail.com>
2023-07-16 18:33:51 +0300

    artık yıl bulma programı tamamlandı

---
+++ src/ArtikYil/ArtikYil.java
@@ -0,0 +1,15 @@
+package ArtikYil;
+
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Yıl Giriniz :");
+        int year = inp.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0)||(year % 100 == 0 && year % 400 == 0)) {
+            System.out.print(year + "= Artık yıldır.");
+        }else System.out.print(year + "= Artık Yıl Değildir.");
+    }
+}
